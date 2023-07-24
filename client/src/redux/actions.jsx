7cb0cabd3678129268b041a7f8773deb253e0b79
andresisfx <andresrodriguezwg@gmail.com>
2023-07-24 15:30:44 -0500

    getting recipes

--- client/src/redux/actions.jsx
+++ client/src/redux/actions.jsx
@@ -1,3 +1,39 @@
+import axios from "axios"
 const GET_RECIPES = "GET_RECIPES"
 const GET_RECIPESID ="GET_RECIPESID"
+const GET_RECIPESNAME = "GET_RECIPESNAME"
+const GET_DIETS = "GET_DIETS"
+
+
+ export  function getRecipes (){
+    return async function(dispatch){
+      try {
+        const recipes = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=5d3cd5aee1904f55a402ec01d5b3380b&addRecipeInformation=true&number=100").data;
+    
+        dispatch({
+            type:GET_RECIPES,
+            payload:recipes
+        })
+      } catch (error) {
+        alert({error:error.message})
+      }  
+    }
+ }
+
+ export function getDiets (){
+  return async function (dispatch){
+    try {
+      const diets = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=5d3cd5aee1904f55a402ec01d5b3380b&addRecipeInformation=true&number=100").data
+      dispatch({
+        type:GET_DIETS,
+        payload:response
+      })
+    } catch (error) {
+      
+    }
+  }
+ }
+
+      
+
  
--- client/src/redux/reducer.jsx
+++ client/src/redux/reducer.jsx
@@ -1,6 +1,18 @@
+import {GET_RECIPES} from "./actions"
 
+let initialState = {
+    allRecipes:[],
 
-function rootReducer (){
+}
 
+function rootReducer (state = initialState,action){
+   switch (action.type) {
+    case value:
+        
+        break;
+   
+    default:
+        break;
+   }
 }
 export default rootReducer;
