9dd88aeeb3add9ea13437922cbda474c35f36609
andresisfx <andresrodriguezwg@gmail.com>
2023-07-29 14:07:51 -0500

    form working

--- client/src/views/form/Form.jsx
+++ client/src/views/form/Form.jsx
@@ -2,17 +2,25 @@ import React  from 'react'
 import { useState,useEffect } from "react"
 import { useDispatch,useSelector } from 'react-redux'
 import { getDiets } from '../../redux/actions';
+import { Link } from 'react-router-dom';
 function Form() {
   const allDiets = useSelector((state)=>state.allDiets);
   const dispatch = useDispatch();
-  const {errors,setErrors}=useState();
-  const [input,setInput] = useState({
+  const [errors,setErrors]=useState({
     name:"",
     image:"",
     healthScore:"",
-    summary:"",
+    summaryOfDish:"",
     diets:[],
-    instructions:[],
+    stepByStep:[]
+  });
+  const [input,setInput] = useState({ 
+    name:"",
+    image:"",
+    healthScore:"",
+    summaryOfDish:"",
+    diets:[],
+    stepByStep:[],
     instruction:""
 
   })
@@ -24,13 +32,14 @@ function Form() {
   const handleChange= (event)=>{
     
     setInput({...input,[event.target.name]:event.target.value})
+    validate(event)
   }
   const addInstruction = (event)=>{
     event.preventDefault()
     if(input.instruction.length>0){
-      const savedInstructions = input.instructions;
-      savedInstructions.push({name:input.instruction})
-      setInput({...input,[input.instructions]:savedInstructions})
+      const savedstepByStep = input.stepByStep;
+      savedstepByStep.push({name:input.instruction})
+      setInput({...input,[input.stepByStep]:savedstepByStep})
       setInput({...input,instruction:""})
 
     }
@@ -45,39 +54,78 @@ function Form() {
     }
     return checked
   }
-  const handleChangeDiets = (id)=>{
-    setInput({...input,diets:handlerCheckBox(id,[...input.diets])})
+  const handleChangeDiets = (name)=>{
+    setInput({...input,diets:handlerCheckBox(name,[...input.diets])})
   }
-  const isChecked= (id)=>{
-    return input.diets.includes(id)
+  const isChecked= (name)=>{
+    return input.diets.includes(name)
   }
-  console.log(input)
+  const validate=(event)=>{
+    const value= event.target.value
+    const property = event.target.name
+    if(property=== "name"){
+      const regex=/^[A-Za-z\s]+$/;
+      if(!regex.test(value)){
+        setErrors({...errors,name:"only leters are allowed"})
+      }
+     
+      }
+      if(property=== "image"){
+        const regex=/^https?:\/\/[\w-]+(\.[\w-]+)+[/#?]?.*$/;
+        if(!regex.test(value)){
+          setErrors({...errors,image:"invalid URL"})
+        }
+       
+        }
+        if(property=== "healthScore"){
+          
+          if(Number.isInteger(value)){
+            setErrors({...errors,image:"invalid URL"})
+          }
+         
+          }
+    }
+    
+    
+  
+   
+  // const onSubmit=(event)=>{
+  //  event.preventDefault();
+
+  // }
+  console.log(errors)
   return (
     <form action="">
       <div>
        <input type="text" placeholder="Name: (max: 100 letters)" name="name" value={input.name} onChange={handleChange} />
        <input type="text" placeholder="image url" name="image" value={input.image} onChange={handleChange} />
        <input type="number" placeholder="HealthScore: (min:1 max:100 points )" name="healthScore" value={input.healthScore} onChange={handleChange} />
-       <textarea placeholder="Summary: (max: 250 letters)" name='summary' value={input.summary} onChange={handleChange}  cols="30" rows="10"></textarea>
+       <textarea placeholder="summaryOfDish: (max: 250 letters)" name='summaryOfDish' value={input.summaryOfDish} onChange={handleChange}  cols="30" rows="10"></textarea>
       </div>
       <div>
         <h3>Choose yourdiet type</h3>
         {Array.isArray(allDiets)&&allDiets.map((diet)=>(
-          <div key={diet.id}>
+          <div key={diet.name}>
             <input
              type="checkbox"
-             id={`check-${diet.id}`}
-             onChange={()=>handleChangeDiets()} // ()=> it allows that handlerDiets will be executed only when the event occurs, instead of executing it immediately.
-             defaultChecked={isChecked(diet.id)} />
-             <label  htmlFor={`check-${diet.id}`}>{diet.name}</label>
+             id={`check-${diet.name}`}
+             onChange={()=>handleChangeDiets(diet.name)} // ()=>  handlerDiets will be executed only when the event occurs, instead of executing it immediately.
+             defaultChecked={isChecked(diet.name)} />
+             <label  htmlFor={`check-${diet.name}`}>{diet.name}</label>
           </div>
         ))}
       </div>
       <div>
-        <h3>Add instructions: </h3>
+        <h3>Add stepByStep: </h3>
         <input type="text" name='instruction' value={input.instruction} onChange={handleChange} />
         <button onClick={addInstruction}>Add</button>
-        {input.instructions.map((ins,index)=><input value={ins.name} key={index} disabled/>)}
+        {input.stepByStep.map((ins,index)=><input value={ins.name} key={index} disabled/>)}
+      </div>
+      <div>
+        <Link to="/home">
+          <button>Back home</button>
+        </Link>
+          <button>Create recipe</button> 
       </div>
     </form>
   )
